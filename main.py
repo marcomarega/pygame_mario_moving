6447a8160e2e64f20225d6a5189246af9e7e115a
Markusssian <mareshkakrakazabr@gmail.com>
2022-12-29 21:52:24 +1000

    Теперь Марио перемещается по тору

--- main.py
+++ main.py
@@ -4,13 +4,73 @@ import sys
 import os
 
 
-pygame.init()
-SIZE = WIDTH, HEIGHT = 500, 500
-screen = pygame.display.set_mode(SIZE)
-pygame.display.set_caption("Перемещение героя")
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, image, x, y):
+        super(Tile, self).__init__()
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
-clock = pygame.time.Clock()
-FPS = 50
+    def set_pos(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+
+
+class TileDesc:
+    def __init__(self, image):
+        self.image = image
+
+    def get_tile(self, x, y):
+        return Tile(self.image, x, y)
+
+
+class Character(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Character, self).__init__(character_group)
+        self.image = player_image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def set_pos(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+
+
+class Toroid:
+    def __init__(self, level, level_x, level_y):
+        self.level = level
+        self.level_x = level_x
+        self.level_y = level_y
+
+        self.tile_descs = list()
+        for row in self.level:
+            self.tile_descs.append(list())
+            for el in row:
+                if el != '@':
+                    self.tile_descs[-1].append(TileDesc(tiles_image[el]))
+                else:
+                    self.tile_descs[-1].append(TileDesc(tiles_image['.']))
+
+        self.shift_x = 0
+        self.shift_y = 0
+
+    def set_d_shift(self, dx, dy):
+        self.shift_x += dx
+        self.shift_y += dy
+
+    def get_tile_desc(self, x, y):
+        return self.tile_descs[(y - self.shift_y) % self.level_y][(x - self.shift_x) % self.level_x]
+
+    def get_view(self, count_x, count_y):
+        view_group = pygame.sprite.Group()
+        for i in range(count_x):
+            for j in range(count_y):
+                tile = self.tile_descs[(j - self.shift_y) % self.level_y][(i - self.shift_x) % self.level_x].\
+                    get_tile(i * TILE_SIZE, j * TILE_SIZE)
+                tile.add(view_group)
+        return view_group
 
 
 def load_image(name, colorkey=None):
@@ -30,43 +90,6 @@ def load_image(name, colorkey=None):
     return image
 
 
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
-def start_screen():
-    intro_text = ["MARIO: La gioca italiana", "",
-                  "Правила игры",
-                  "Перемещать Mario можно",
-                  "только по траве,",
-                  "на коробки залезать нельзя"]
-
-    screen.fill(pygame.Color("white"))
-    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 25)
-    text_coord = 250
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 250
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
-        pygame.display.flip()
-        clock.tick(FPS)
-
-
 def load_level(filename):
     filename = "data/" + filename
     # читаем уровень, убирая символы перевода строки
@@ -80,113 +103,111 @@ def load_level(filename):
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
-tile_images = {
-    'wall': load_image('box.png'),
-    'empty': load_image('grass.png')
-}
-player_image = load_image('mario.png')
-
-tile_width = tile_height = 50
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super(Tile, self).__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super(Player, self).__init__(player_group, all_sprites)
-        self.image = player_image
-        self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-
-
-# основной персонаж
-player = None
-
-# группы спрайтов
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-
-
 def generate_level(level):
     new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#':
-                Tile('wall', x, y)
-            elif level[y][x] == '@':
-                Tile('empty', x, y)
-                new_player = Player(x, y)
+    for x in range(len(level)):
+        for y in range(len(level[x])):
+            if level[x][y] == '@':
+                new_player = Character(y * TILE_SIZE, x * TILE_SIZE)
     # вернем игрока, а также размер поля в клетках
-    return new_player, x, y
+    return new_player, y + 1, x + 1
 
 
-class Camera:
-    # зададим начальный сдвиг камеры
-    def __init__(self):
-        self.dx = 0
-        self.dy = 0
+SIZE = WIDTH, HEIGHT = 450, 450
+TILE_SIZE = 50
 
-    # сдвинуть объект obj на смещение камеры
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
+pygame.init()
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("Перемещение героя. Новый уровень")
 
-    # позиционировать камеру на объекте target
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+clock = pygame.time.Clock()
+FPS = 50
 
 
-class Level:
-    def __init__(self):
-        self.level = load_level('map.txt')
-        self.player, self.level_x, self.level_y = generate_level(self.level)
+tiles_image = {
+    '.': load_image("grass.png"),
+    '#': load_image("box.png"),
+}
+player_image = load_image("mario.png")
 
-        self.camera = Camera()
+character_group = pygame.sprite.Group()
 
-    def run(self):
-        running = True
-        while running:
-            dx, dy = 0, 0
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    terminate()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        dx = -1
-                    if event.key == pygame.K_RIGHT:
-                        dx = 1
-                    if event.key == pygame.K_UP:
-                        dy = -1
-                    if event.key == pygame.K_DOWN:
-                        dy = 1
-            if 0 <= self.player.pos_x + dx < self.level_x \
-                    and 0 <= self.player.pos_y + dy < self.level_y \
-                    and self.level[self.player.pos_y + dy][self.player.pos_x + dx] \
-                    != "#":
-                self.player.pos_x += dx
-                self.player.pos_y += dy
-                self.player.rect.x += dx * tile_width
-                self.player.rect.y += dy * tile_height
-            self.camera.update(self.player)
-            for sprite in all_sprites:
-                self.camera.apply(sprite)
-            screen.fill((0, 0, 0))
-            tiles_group.draw(screen)
-            player_group.draw(screen)
-            pygame.display.flip()
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["MARIO: La gioca italiana", "",
+                  "Правила игры",
+                  "Перемещать Mario можно",
+                  "только по траве,",
+                  "на коробки залезать",
+                  "нельзя"]
+
+    screen.fill(pygame.Color("white"))
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 25)
+    text_coord = 250
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 220
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def level(map_filename):
+    level = load_level(map_filename)
+    character, level_x, level_y = generate_level(level)
+    toroid = Toroid(level, level_x, level_y)
+    shift_x = -(character.rect.x + TILE_SIZE // 2 - WIDTH // 2) // TILE_SIZE
+    shift_y = -(character.rect.y + TILE_SIZE // 2 - HEIGHT // 2) // TILE_SIZE
+    # shift_x, shift_y = 0, 0
+    character.set_pos(character.rect.x + shift_x * TILE_SIZE + 15, character.rect.y + shift_y * TILE_SIZE + 5)
+    toroid.set_d_shift(shift_x, shift_y)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                dx, dy = 0, 0
+                if event.key == pygame.K_LEFT:
+                    dx = -1
+                if event.key == pygame.K_RIGHT:
+                    dx = 1
+                if event.key == pygame.K_UP:
+                    dy = -1
+                if event.key == pygame.K_DOWN:
+                    dy = 1
+                x = (character.rect.x - 15) // TILE_SIZE + dx
+                y = (character.rect.y - 5) // TILE_SIZE + dy
+                if toroid.get_tile_desc(x, y).image != tiles_image['#']:
+                    toroid.set_d_shift(-dx, -dy)
+
+        screen.fill((0, 0, 0))
+        view_group = toroid.get_view(WIDTH // TILE_SIZE, HEIGHT // TILE_SIZE)
+        view_group.draw(screen)
+        character_group.draw(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
 
 
 if __name__ == "__main__":
     start_screen()
-    level = Level()
-    level.run()
+    level("minimap.txt")
